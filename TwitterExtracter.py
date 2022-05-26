44929ef021b8d2ed8e682f0981e96996d136fca7
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-05-26 15:34:42 +0900

    Update

--- TwitterExtracter.py
+++ TwitterExtracter.py
@@ -6,12 +6,11 @@ API_KEY = config["API_KEY"]
 API_SECRET = config["API_SECRET"]
 ACCESS_TOKEN = config["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = config["ACCESS_TOKEN_SECRET"]
-auth = tweepy.OAuthHandler(API_KEY, API_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-api = tweepy.API(auth)
-tweets = api.search_tweets(q=["Python"], count=10)
+client = tweepy.Client(None, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-for tweet in tweets:
+tweets = client.search_recent_tweets(query = "Python", max_results = 10)
+
+for tweet in tweets.data:
     print("-----------------")
     print(tweet.text)
