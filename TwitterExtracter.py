554480cf8041665784880dac72bb06c34da50c1e
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-05-27 09:08:49 +0900

    Update

--- TwitterExtracter.py
+++ TwitterExtracter.py
@@ -2,15 +2,16 @@ import json
 import tweepy
 
 config = json.load(open("./config.json", "r"))
+BEARER_TOKEN = config["BEARER_TOKEN"]
 API_KEY = config["API_KEY"]
 API_SECRET = config["API_SECRET"]
 ACCESS_TOKEN = config["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = config["ACCESS_TOKEN_SECRET"]
 
-client = tweepy.Client(None, API_KEY, API_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET,
+                       ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-tweets = client.search_recent_tweets(query = "Python", max_results = 10)
-
+tweets = client.search_recent_tweets(query="Python", max_results=10)
 for tweet in tweets.data:
     print("-----------------")
     print(tweet.text)
