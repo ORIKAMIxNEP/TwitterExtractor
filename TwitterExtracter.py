b7e851ab7f4e416e095faa71c10ea8f6b8053a9a
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-06-25 20:40:03 +0900

    Update

--- TwitterExtracter.py
+++ TwitterExtracter.py
@@ -1,39 +1,57 @@
+from flask import Flask
+from flask_cors import CORS
 import json
 import tweepy
 
+app = Flask(__name__)
+CORS(
+    app,
+    supports_credentials=True
+)
 
-config = json.load(open("./config.json", "r"))
-BEARER_TOKEN = config["BEARER_TOKEN"]
-API_KEY = config["API_KEY"]
-API_SECRET = config["API_SECRET"]
-ACCESS_TOKEN = config["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = config["ACCESS_TOKEN_SECRET"]
-client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET,
-                       ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-searchText = "タコピーの原罪"
-next_token = ""
-allTweetsCount = 0
-extractedTweetsCount = 0
-for i in range(5):
-    if i == 0:
-        tweets = client.search_recent_tweets(
-            query=searchText, max_results=100, tweet_fields=["entities", "lang", "public_metrics"])
-    else:
-        tweets = client.search_recent_tweets(
-            query=searchText, max_results=10, next_token=next_token, tweet_fields=["entities", "lang", "public_metrics"])
-    for tweet in tweets.data:
-        if tweet.lang == "ja" and (tweet.entities is None or ("mentions" not in tweet.entities and "urls" not in tweet.entities)):
-            print("-------------------------------------------")
-            print(tweet.text)
-            print("fav:" + str(tweet.public_metrics["like_count"]))
-            extractedTweetsCount += 1
-        allTweetsCount += 1
-    if "next_token" not in tweets.meta:
-        break
-    else:
-        next_token = tweets.meta["next_token"]
+@app.route("/", methods=["GET"])
+def TwitterExtracter():
+    config = json.load(open("./config.json", "r"))
+    BEARER_TOKEN = config["BEARER_TOKEN"]
+    API_KEY = config["API_KEY"]
+    API_SECRET = config["API_SECRET"]
+    ACCESS_TOKEN = config["ACCESS_TOKEN"]
+    ACCESS_TOKEN_SECRET = config["ACCESS_TOKEN_SECRET"]
+    client = tweepy.Client(BEARER_TOKEN, API_KEY, API_SECRET,
+                           ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
-print("-------------------------------------------")
-print("AllTweet:" + str(allTweetsCount))
-print("ExtractedTweet:" + str(extractedTweetsCount))
+    searchText = "タコピーの原罪"
+    next_token = ""
+    allTweetsCount = 0
+    extractedTweetsCount = 0
+    for i in range(5):
+        if i == 0:
+            tweets = client.search_recent_tweets(
+                query=searchText, max_results=100, tweet_fields=["entities", "lang", "public_metrics"])
+        else:
+            tweets = client.search_recent_tweets(
+                query=searchText, max_results=100, next_token=next_token, tweet_fields=["entities", "lang", "public_metrics"])
+        TweetsData = {"tweets": []}
+        for tweet in tweets.data:
+            if tweet.lang == "ja" and (tweet.entities is None or ("mentions" not in tweet.entities and "urls" not in tweet.entities)):
+                print("-------------------------------------------")
+                print(tweet.text)
+                print("fav:" + str(tweet.public_metrics["like_count"]))
+                TweetsData["tweets"].append(
+                    dict({"tweet": tweet.text, "favorite": tweet.public_metrics["like_count"]}))
+                extractedTweetsCount += 1
+            allTweetsCount += 1
+        if "next_token" not in tweets.meta:
+            break
+        else:
+            next_token = tweets.meta["next_token"]
+
+    print("-------------------------------------------")
+    print("AllTweet:" + str(allTweetsCount))
+    print("ExtractedTweet:" + str(extractedTweetsCount))
+    return TweetsData
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=50001, debug=True)
---
+++ static/script.js
@@ -0,0 +1,13 @@
+fetch("http://127.0.0.1:50001/") // API呼び出し
+  .then(function (data) {
+    return data.json(); // json形式で読み込む
+  })
+  .then(function (TweetsData) {
+    let tweetsHTML = ""; // HTMLに入れるデータの変数
+    for (let i = 0; i < TweetsData.tweets.length; i++) {
+      console.log(TweetsData.tweets[i].tweet); // ツイートの文章
+      console.log(TweetsData.tweets[i].favorite); // ツイートのいいね数
+      tweetsHTML += "<p>" + TweetsData.tweets[i].tweet + "<p>"; // ツイートデータをHTML用データに格納
+      tweets.innerHTML = tweetsHTML; // HTML用データをHTML(id:tweets)に反映
+    }
+  });
