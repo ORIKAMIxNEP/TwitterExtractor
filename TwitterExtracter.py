cbdac3e939b998205acb32cec5d045801b635535
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-13 11:24:09 +0900

    Update

--- TwitterExtracter.py
+++ TwitterExtracter.py
@@ -38,7 +38,7 @@ def TwitterExtracter():
                 print("-------------------------------------------")
                 print(tweet.text)
                 print("fav:" + str(tweet.public_metrics["like_count"]))
-                TweetsData["tweets"].append(
+                TweetsData["tweets"].update(
                     dict({"tweet": tweet.text, "favorite": tweet.public_metrics["like_count"]}))
                 extractedTweetsCount += 1
             allTweetsCount += 1
