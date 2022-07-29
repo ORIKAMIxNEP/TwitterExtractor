36f8ac122a5521b25a63494303719576218f9f96
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-07-29 13:42:36 +0900

    Update

--- TwitterExtracter.py
+++ TwitterExtracter.py
@@ -1,10 +1,8 @@
-from operator import imod
-from random import random
 from flask import Flask, request
 from flask_cors import CORS
 import json
+from random import random
 import tweepy
-import random
 
 app = Flask(__name__)
 CORS(
@@ -29,30 +27,30 @@ def TwitterExtracter():
     allTweetsCount = 0
     extractedTweetsCount = 0
     TweetsData = {"tweets": []}
-    # for i in range(5):
-    #     if i == 0:
-    #         tweets = client.search_recent_tweets(
-    #             query=searchText, max_results=100, tweet_fields=["entities", "lang", "public_metrics"])
-    #     else:
-    #         tweets = client.search_recent_tweets(
-    #             query=searchText, max_results=100, next_token=next_token, tweet_fields=["entities", "lang", "public_metrics"])
-    #     for tweet in tweets.data:
-    #         if tweet.lang == "ja" and (tweet.entities is None or ("mentions" not in tweet.entities and "urls" not in tweet.entities)):
-    #             print("-------------------------------------------")
-    #             print(tweet.text)
-    #             print("favorite:" + str(tweet.public_metrics["like_count"]))
-    #             TweetsData["tweets"].append(
-    #                 dict({"tweet": tweet.text, "favorite": tweet.public_metrics["like_count"]}))
-    #             extractedTweetsCount += 1
-    #         allTweetsCount += 1
-    #     if "next_token" not in tweets.meta:
-    #         break
-    #     else:
-    #         next_token = tweets.meta["next_token"]
+    for i in range(5):
+        if i == 0:
+            tweets = client.search_recent_tweets(
+                query=searchText, max_results=100, tweet_fields=["entities", "lang", "public_metrics"])
+        else:
+            tweets = client.search_recent_tweets(
+                query=searchText, max_results=100, next_token=next_token, tweet_fields=["entities", "lang", "public_metrics"])
+        for tweet in tweets.data:
+            if tweet.lang == "ja" and (tweet.entities is None or ("mentions" not in tweet.entities and "urls" not in tweet.entities)):
+                print("-------------------------------------------")
+                print(tweet.text)
+                print("favorite:" + str(tweet.public_metrics["like_count"]))
+                TweetsData["tweets"].append(
+                    dict({"tweet": tweet.text, "favorite": tweet.public_metrics["like_count"]}))
+                extractedTweetsCount += 1
+            allTweetsCount += 1
+        if "next_token" not in tweets.meta:
+            break
+        else:
+            next_token = tweets.meta["next_token"]
 
-    for i in range(100):
-        TweetsData["tweets"].append(
-            dict({"tweet": "テストツイート"+str(i+1), "favorite": random.randint(0, 1000)}))
+    # for i in range(100):
+    #     TweetsData["tweets"].append(
+    #         dict({"tweet": "テストツイート"+str(i+1), "favorite": random.randint(0, 1000)}))
 
     print("----------------------------------------")
     print("AllTweet:" + str(allTweetsCount))
