72c764f52cc27fcfb659eec246c244dae8ffa6a6
Taiki Orikami <65590892+ORIKAMIxNEP@users.noreply.github.com>
2022-09-16 07:21:22 +0900

    JSファイル追加

---
+++ static/api_calls.js
@@ -0,0 +1,18 @@
+function search() {
+  fetch(
+    "http://172.31.50.223:50001/?word=" +
+      document.getElementById("search").value
+  )
+    .then(function (data) {
+      return data.json();
+    })
+    .then(function (TweetsData) {
+      let tweetsHTML = "";
+      for (let i = 0; i < TweetsData.tweets.length; i++) {
+        console.log(TweetsData.tweets[i].tweet);
+        console.log(TweetsData.tweets[i].favorite);
+        tweetsHTML += "<p>" + TweetsData.tweets[i].tweet + "<p>";
+        tweets.innerHTML = tweetsHTML;
+      }
+    });
+}
