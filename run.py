584430426b876729a6fa8c992ca05f283a3b4b22
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-18 06:57:33 +0900

    Flaskファイルの統合

---
+++ run.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from ExtractTwitter import ExtractTwitter
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def Web():
+    return render_template("index.html")
+
+
+@app.route("/extract_twitter", methods=["GET"])
+def CallExtractTwitter():
+    return ExtractTwitter(request.args.get("word"))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80, debug=True)
